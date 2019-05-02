2500834f34bf07af0efcf798d5796fa288278c6a
Barry Richards <barry@br-web.io>
2019-05-02 19:27:25 +0100

    tweak gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,93 +1,69 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    postcss: {
-      dev: {
+
+  require('load-grunt-tasks')(grunt);
+  
+    grunt.initConfig({
+      postcss: {
         options: {
           map: true, // inline sourcemaps
+  
           processors: [
-            require("precss")(), // deal with SASS gubbins
-            require("autoprefixer")({ browsers: "last 2 versions" }) // add vendor prefixes
+            require('pixrem')(), // add fallbacks for rem units 
+            require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes 
+            require('cssnano')() // minify the result 
           ]
         },
-        src: "css/input/style.css",
-        dest: "css/style.css"
+        dist: {
+          src: 'css/main.css'
+        }
       },
-      prod: {
-        options: {
-          processors: [
-            require("pixrem")(), // add fallbacks for rem units
-            require("autoprefixer")({ browsers: "last 2 versions" }), // add vendor prefixes
-            require("cssnano")(), // minify the result
-            require("precss")() // deal with SASS gubbins
-          ]
-        },
-        src: "css/input/style.css",
-        dest: "dest/css/style.css"
-      }
-    },
-    uglify: {
-      files: {
-        src: "js/scripts.js",
-        dest: "dest/js/scripts.js"
-      }
-    },
-    htmlmin: {
-      options: {
-        removeComments: true,
-        collapseWhitespace: true
-      },
-      files: {
-        src: "index.html",
-        dest: "dest/index.html"
-      }
-    },
-    imagemin: {
-      dynamic: {
-        files: [
-          {
-            expand: true,
-            cwd: "assets/images/",
-            src: ["**/*.{png,jpg,gif}"],
-            dest: "dest/assets/images/"
+      sass: {
+        dist: {
+          files: {
+            'css/main.css': 'sass/main.scss'
           }
-        ]
-      }
-    },
-    browserSync: {
-      bsFiles: {
-        src: ["css/style.css", "index.html"]
+        }
       },
-      options: {
-        watchTask: true,
-        proxy: "build:8888"
-      }
-    },
-    watch: {
-      css: {
-        files: ["css/input/*.css"],
-        tasks: ["postcss:dev"]
-      }
-    }
-  });
+      svgmin: {
+        options: {
+          plugins: [
+            {
+              removeViewBox: true
+            },
+            {
+              removeUselessStrokeAndFill: true
+            },
+            {
+              cleanupIDs:false
+            },
+          ]
+        },
+        dist: {
+          files: {
+            'assets/svg/icons.svg': 'assets/svg/input/*.svg'
+          }
+        }
+      },
+      watch: {
+        sass: {
+          files: ['sass/*.scss'],
+          tasks: ['sass'],
+         }
+      },
+      browserSync: {
+        bsFiles: {
+          src: ["_site/css/main.css", "_site/*.html"]
+        },
+        options: {
+          watchTask: true,
+          proxy: "http://localhost:4000"
+        }
+      },
+    });
 
-  grunt.registerTask("dev", ["browserSync", "watch"]);
-  grunt.registerTask("build", [
-    "uglify",
-    "htmlmin",
-    "imagemin",
-    "postcss:prod"
-  ]);
 
-  // postCSS
-  grunt.loadNpmTasks("grunt-postcss");
-  // uglify
-  grunt.loadNpmTasks("grunt-contrib-uglify-es");
-  // minify html
-  grunt.loadNpmTasks("grunt-contrib-htmlmin");
-  // minify images
-  grunt.loadNpmTasks("grunt-contrib-imagemin");
-  // watch task
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  // browser reload
-  grunt.loadNpmTasks("grunt-browser-sync");
-};
+    grunt.registerTask("dev", ["browserSync", "watch"]);
+    grunt.registerTask('svg', ['svgmin']);
+  
+
+  };
